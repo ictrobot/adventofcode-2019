4ac114bfebdf8d96e8931b3e954fc766601d340c
ictrobot <ictrobot@outlook.com>
2019-12-05 15:27:29 +0000

    Day 5 cleanup

--- day5/task2.py
+++ day5/task2.py
@@ -1,10 +1,6 @@
 import functools
 
 
-class InvalidState(Exception):
-    pass
-
-
 def resolve_parameter(pc, memory, parameter_number, write):
     val = memory[pc + parameter_number]
     opcode = memory[pc]
@@ -14,22 +10,26 @@ def resolve_parameter(pc, memory, parameter_number, write):
         return functools.partial(memory.__setitem__ if write else memory.__getitem__, val)
     elif mode == 1:  # immediate
         if write:
-            raise InvalidState()
+            raise ValueError()
         return lambda: val
 
 
-def opcode1_plus(pc, memory):
-    operand1 = resolve_parameter(pc, memory, 1, False)()
-    operand2 = resolve_parameter(pc, memory, 2, False)()
-    resolve_parameter(pc, memory, 3, True)(operand1 + operand2)
-    return pc + 4
+def op_template(func):
+    def operation(pc, memory):
+        operand1 = resolve_parameter(pc, memory, 1, False)()
+        operand2 = resolve_parameter(pc, memory, 2, False)()
+        resolve_parameter(pc, memory, 3, True)(func(operand1, operand2))
+        return pc + 4
+    return operation
 
 
-def opcode2_mult(pc, memory):
-    operand1 = resolve_parameter(pc, memory, 1, False)()
-    operand2 = resolve_parameter(pc, memory, 2, False)()
-    resolve_parameter(pc, memory, 3, True)(operand1 * operand2)
-    return pc + 4
+def op_conditional_jump(cond):
+    def operation(pc, memory):
+        if cond(resolve_parameter(pc, memory, 1, False)()):
+            return resolve_parameter(pc, memory, 2, False)()
+        else:
+            return pc + 3
+    return operation
 
 
 def opcode3_input(pc, memory):
@@ -48,48 +48,25 @@ def opcode4_output(pc, memory):
     return pc + 2
 
 
-def opcode5_jump_true(pc, memory):
-    if resolve_parameter(pc, memory, 1, False)():
-        return resolve_parameter(pc, memory, 2, False)()
-    else:
-        return pc + 3
+OPCODES = {
+    1: op_template(lambda x, y: x + y),                     # +
+    2: op_template(lambda x, y: x * y),                     # *
+    3: opcode3_input,                                       # input
+    4: opcode4_output,                                      # output
+    5: op_conditional_jump(lambda x: x),                    # jump if true
+    6: op_conditional_jump(lambda x: not x),                # jump if false
+    7: op_template(lambda x, y: 1 if x < y else 0),         # less than
+    8: op_template(lambda x, y: 1 if x == y else 0)         # equal to
+}
 
 
-def opcode6_jump_false(pc, memory):
-    if not resolve_parameter(pc, memory, 1, False)():
-        return resolve_parameter(pc, memory, 2, False)()
-    else:
-        return pc + 3
-
-
-def opcode7_less_than(pc, memory):
-    operand1 = resolve_parameter(pc, memory, 1, False)()
-    operand2 = resolve_parameter(pc, memory, 2, False)()
-    resolve_parameter(pc, memory, 3, True)(1 if operand1 < operand2 else 0)
-    return pc + 4
-
-
-def opcode8_equals(pc, memory):
-    operand1 = resolve_parameter(pc, memory, 1, False)()
-    operand2 = resolve_parameter(pc, memory, 2, False)()
-    resolve_parameter(pc, memory, 3, True)(1 if operand1 == operand2 else 0)
-    return pc + 4
-
-
-def run(memory):
+def run_program(memory):
     pc = 0
-    opcodes = {1: opcode1_plus, 2: opcode2_mult,
-               3: opcode3_input, 4: opcode4_output,
-               5: opcode5_jump_true, 6: opcode6_jump_false,
-               7: opcode7_less_than, 8: opcode8_equals}
-    while pc < len(memory):
-        opcode = memory[pc] % 100
-        if opcode == 99:
-            break
-        pc = opcodes[opcode](pc, memory)
+    while pc < len(memory) and memory[pc] % 100 != 99:
+        pc = OPCODES[memory[pc] % 100](pc, memory)
 
 
 if __name__ == "__main__":
     with open(__file__.replace(".py", ".in"), "r") as in_file:
         data = [int(x.strip()) for x in in_file.read().split(",")]
-    run(data)
+    run_program(data)
